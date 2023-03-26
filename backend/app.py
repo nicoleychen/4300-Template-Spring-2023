d0f920f5ecb109ecc5531b8d568520b3520c3f0e
Aurora Mu <aurorayuqiaomu@gmail.com>
2023-03-26 13:12:05 -0400

    testing

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = "Xiaoniao0718"
+MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "kaggleperfumedb"
 
