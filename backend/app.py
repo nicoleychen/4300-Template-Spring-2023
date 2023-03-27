58de300e38a7b0974c2d182481dc1ab2b8152704
Melissa Chen <mmc324@cornell.edu>
2023-03-27 18:36:12 -0400

    Files changes

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_USER_PASSWORD = os.getenv('MYSQL_USER_PASSWORD')
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "kardashiandb"
+MYSQL_DATABASE = "findmyfragrance_db"
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
