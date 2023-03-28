d55fe1ffd7b0cf743ea4200742e114389c4475c7
Madi Yeh <mgy6@cornell.edu>
2023-03-27 22:20:21 -0400

    Commented broken line

--- backend/app.py
+++ backend/app.py
@@ -32,7 +32,7 @@ mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
-mysql_engine.load_file_into_db()
+# mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
 CORS(app)
